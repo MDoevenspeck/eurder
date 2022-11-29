984552b38994a3f4febe25bd82d573028cd906bd
MDoevenspeck <mattias.doevenspeck@gmail.com>
2022-11-29 05:52:26 +0100

    initial commit

---
+++ src/main/java/com/switchfully/eurder/EurderApplication.java
@@ -0,0 +1,14 @@
+package com.switchfully.eurder;
+
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurderApplication.class, args);
+	}
+}
